518f5bb119f6a5ec3877dba42d89b510b8dba41d
GothicZhang <imzxz123456@gmail.com>
2018-05-08 19:37:56 +0800

    Add files via upload

---
+++ 1001_ 害死人不偿命的(3n+1)猜想.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int count = 0;
+  int n;
+  cin>>n;
+  while(n != 1){
+    if(n % 2 == 0)
+      n = n / 2;
+    else
+      n = (3 * n + 1) / 2;
+    count++;
+  }
+  cout<<count<<endl;
+  return 0;
+}
